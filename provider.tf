94638667b27a142d8431b9220e5594cf991a197b
Nipun Kumar <nipunpanwar1986@gmail.com>
2025-07-05 01:50:41 +0530

    Update subscription id

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.9.0"
+      version = "4.35.0"
     }
   }
 }
 
 provider "azurerm" {
   features { }
-subscription_id = "5c8f30fc-d02e-4dec-9dff-2a82a0b3a7b9"
+subscription_id = "5434828b-4299-46cb-9433-bee373d4ad6b"
 }
